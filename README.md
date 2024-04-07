9819974cd2273b65582b2c7414e4eecdd7487e77
Dyanesa <canerarslan78@gmail.com>
2024-04-07 23:09:02 +0300

    asd

--- README.md
+++
@@ -1,1 +0,0 @@
-# kurtkoyemlakcisi.github.io
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Skills from "@/containers/Skills/Skills";
+import Blogs from "../containers/Blogs/Blogs";
+
+export default async function Home() {
+  return (
+    <>
+      <Skills />
+      <Blogs />
+    </>
+  );
+}
